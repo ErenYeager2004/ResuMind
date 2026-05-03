757525f22d0810c82132b217c5c4ae5d1a739f96
Debjit Saha <debjitsaha606@gmail.com>
2026-05-03 20:19:22 +0530

    updated authentication

--- ResuMind/app/routes/home.tsx
+++ ResuMind/app/routes/home.tsx
@@ -3,6 +3,9 @@ import { resumes } from "../../constants";
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar.js";
 import ResumeCard from "~/components/ResumeCard";
+import { usePuterStore } from "~/lib/puter";
+import { useLocation, useNavigate } from "react-router";
+import { useEffect } from "react";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "ResuMind" },
@@ -11,10 +14,15 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const {auth} = usePuterStore();
+  const navigate = useNavigate();
+  useEffect( () => {
+    if(!auth.isAuthenticated) navigate("/auth?next=/");
+  }, [auth.isAuthenticated])
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
-      
+
       <section className="main-section">
         <div className="page-heading py-16">
           <h1>Track Your Application & Resume Ratings</h1>
